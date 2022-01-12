7fc12997e19c8531d2497d89fd147c7407ba545d
tarcea <tarcea@gmail.com>
2022-01-12 16:07:20 +0100

    github stats added

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const myFunction = () => {
+  console.log("You scrolled in page");
+}
+
+const mainBody = document.querySelector(".root");
+console.log(mainBody)
+mainBody.onscroll = (() => myFunction());
